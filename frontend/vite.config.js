f4a40465738290384de08ec70975a3e7cabcdbe9
Sambitsinha05 <146098515+Sambitsinha05@users.noreply.github.com>
2026-05-16 00:41:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://tradewise-xa7h.onrender.com',
         changeOrigin: true,
       },
     },
